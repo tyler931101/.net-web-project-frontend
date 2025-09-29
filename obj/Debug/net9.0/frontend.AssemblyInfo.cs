fdb31bdedc892fb0f5b61416f4fec9212027720b
tyler931101 <tylersnow931101@gmail.com>
2025-09-29 05:47:37 -0500

    auth features initial complete

--- obj/Debug/net9.0/frontend.AssemblyInfo.cs
+++ obj/Debug/net9.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+819b55836c22eaebaa3d1222fc50360502f09500")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
